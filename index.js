af48f0270a2f9a38577ad12734583090bac5c08d
kimon-satan <simon@simonkatan.co.uk>
2016-11-18 11:45:33 +0000

    admin and tester

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 //Express initializes app to be a function handler that you can supply to an HTTP server
-var app = require('express')();
+
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //We define a route handler / that gets called when we hit our website home.
-app.get('/hello', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use("/style",express.static(__dirname + "/style"));
+app.use("/libs",express.static(__dirname + "/libs"));
 
-io.on('connection', function(socket){
+//two types of user
+ app.get('/admin', function(req, res){
+   res.sendFile(__dirname + '/admin.html');
+ });
+//
+ app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+ });
+
+
+//io is everyone
+//socket is this user
+
+io.on('connection', function(socket)
+{
+
   console.log('a user connected');
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+  socket.on('command', function(msg)
+  {
+
+    if(msg[0] == "modechange" && msg.length > 1)
+    {
+      socket.broadcast.emit('modechange', msg[1]);
+    }
+
+    console.log('command: ' + msg);
   });
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function()
+  {
     console.log('user disconnected');
   });
+
 });
 
 //We make the http server listen on port 3000.
