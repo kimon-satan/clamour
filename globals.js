ecf632fe5cbba0a57345b83a0be62bdc3b8e0b20
kimon-satan <simon@simonkatan.co.uk>
2017-11-26 20:30:32 +0000

    fixed server bug

--- globals.js
+++ globals.js
@@ -8,7 +8,7 @@ http = require('http').Server(exports.app);
 
 exports.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || '127.0.0.1',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
@@ -30,6 +30,8 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 
   }
 }
+
+console.log(mongoURL);
 
 //simple db using monk & mongodb
 exports.URL = mongoURL;
